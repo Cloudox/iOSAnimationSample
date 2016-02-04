b84e26619b26e63fff4f852ae4b986be94ececac
Cloudox <cloudox@foxmail.com>
2016-02-04 22:39:53 +0800

    Initial Commit

---
+++ iOSAnimationSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOSAnimationSample
+//
+//  Created by Cloudox on 16/2/4.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iOSAnimationSample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  iOSAnimationSample
+//
+//  Created by Cloudox on 16/2/4.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
