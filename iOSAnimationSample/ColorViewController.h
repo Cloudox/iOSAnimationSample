da657840a05f04af04131beacb9e67fe8ea640a1
Cloudox <cloudox@foxmail.com>
2016-02-05 19:31:12 +0800

    完成颜色变化动画

---
+++ iOSAnimationSample/ColorViewController.h
@@ -0,0 +1,13 @@
+//
+//  ColorViewController.h
+//  iOSAnimationSample
+//
+//  Created by Cloudox on 16/2/5.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ColorViewController : UIViewController
+
+@end
---
+++ iOSAnimationSample/ColorViewController.m
@@ -0,0 +1,38 @@
+//
+//  ColorViewController.m
+//  iOSAnimationSample
+//
+//  Created by Cloudox on 16/2/5.
+//  Copyright © 2016年 Cloudox. All rights reserved.
+//
+
+#import "ColorViewController.h"
+
+@interface ColorViewController ()
+@property (strong, nonatomic) IBOutlet UIView *blueSquare;// 蓝色方块
+@property (strong, nonatomic) IBOutlet UILabel *nameLabel;// 名字label
+
+@end
+
+@implementation ColorViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    [UIView animateWithDuration:1 animations:^{
+        self.blueSquare.backgroundColor = [UIColor redColor];
+        self.nameLabel.textColor = [UIColor whiteColor];
+    }];
+}
+
+@end
